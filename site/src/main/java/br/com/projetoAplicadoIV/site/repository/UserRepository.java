a52cb2e5180a99b56695fbc8c76254a5ec3b1db3
LUCAS SASSO MACHADO <lucas_sasso-machado@estudante.sc.senai.br>
2022-10-18 21:51:58 -0300

    Método para salvar. NÃO TESTADO AINDA.

--- site/src/main/java/br/com/projetoAplicadoIV/site/repository/UserRepository.java
+++ site/src/main/java/br/com/projetoAplicadoIV/site/repository/UserRepository.java
@@ -2,9 +2,15 @@ package br.com.projetoAplicadoIV.site.repository;
 
 import br.com.projetoAplicadoIV.site.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Query(value = "from User where cpf = :cpf")
+    Optional<User> findByCPF(@Param("cpf") String cpf);
 }
