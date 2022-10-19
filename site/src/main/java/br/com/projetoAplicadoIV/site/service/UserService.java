6985ea204f5dd9b6880f08cb97f1b60dc9779747
LUCAS SASSO MACHADO <lucas_sasso-machado@estudante.sc.senai.br>
2022-10-18 21:52:22 -0300

    Método para salvar. NÃO TESTADO AINDA.

--- site/src/main/java/br/com/projetoAplicadoIV/site/service/UserService.java
+++ site/src/main/java/br/com/projetoAplicadoIV/site/service/UserService.java
@@ -1,8 +1,11 @@
 package br.com.projetoAplicadoIV.site.service;
 
+import br.com.projetoAplicadoIV.site.entity.User;
 import br.com.projetoAplicadoIV.site.entity.dto.NewUserDTO;
 import br.com.projetoAplicadoIV.site.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -13,7 +16,21 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public void saveUser(NewUserDTO newUser) {
+    public String saveUser(NewUserDTO newUser) {
+        Optional<User> existentUser = userRepository.findByCPF(newUser.getCpf());
 
+        if(existentUser.isPresent()) {
+            return "User already exists.";
+        }
+
+        User user = new User();
+        user.setCpf(newUser.getCpf());
+        user.setEmail(newUser.getEmail());
+        user.setName(newUser.getName());
+        user.setPassword(newUser.getPassword());
+
+        userRepository.save(user);
+
+        return "Success";
     }
 }
