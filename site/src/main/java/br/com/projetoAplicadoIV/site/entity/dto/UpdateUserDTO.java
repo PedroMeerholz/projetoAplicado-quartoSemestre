65d82b204856e7f0d6f9c28ff8d9ed91799b7f84
LucasSM3006 <sasso3006@gmail.com>
2022-10-21 22:53:21 -0300

    Entidade DTO para a atualização de cadastro do usuário.

---
+++ site/src/main/java/br/com/projetoAplicadoIV/site/entity/dto/UpdateUserDTO.java
@@ -0,0 +1,31 @@
+package br.com.projetoAplicadoIV.site.entity.dto;
+
+public class UpdateUserDTO {
+    private String name;
+    private String email;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
