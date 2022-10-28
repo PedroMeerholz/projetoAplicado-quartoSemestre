d780eb1d028c557dc118f095d6ef4a638aa7689e
LucasSM3006 <lucas.sasso@valtech.com>
2022-10-28 17:51:56 -0300

    Inicializado a classe. A parte de verificação ainda está sendo feita, mas já tem a primeira parte quase completa.

--- site/src/main/java/br/com/projetoAplicadoIV/site/utils/CpfUtils.java
+++ site/src/main/java/br/com/projetoAplicadoIV/site/utils/CpfUtils.java
@@ -1,5 +1,43 @@
 package br.com.projetoAplicadoIV.site.utils;
 
 public class CpfUtils {
+    public boolean verifyCpfLength(String cpf) {
+        if(cpf.length() != 11) return true;
+        return false;
+    }
 
+    public boolean verifyCpf(String cpf) {
+        int soma = 0;
+        double uh = 0.0;
+        double resto = 0.0;
+        double restoInt = 0;
+
+        for(int i = 0; i < 9; i++) {
+            soma += Character.getNumericValue(cpf.charAt(i)) * (10 - i);
+        }
+
+        uh = soma * 10 / 11;
+        resto = soma * 10 % 11;
+        if(resto >= 10) {
+            resto = 0;
+            restoInt = 0;
+        }
+
+        restoInt = Math.round(resto);
+
+        System.out.println(uh);
+        System.out.println(resto);
+
+
+
+        return false;
+    }
+
+    public boolean validateFirstDigit() {
+        return true;
+    }
+
+    public boolean validateSecondDigit() {
+        return true;
+    }
 }
