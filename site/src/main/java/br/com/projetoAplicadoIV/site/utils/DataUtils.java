8d3b9d74f81e7c4530804d1a3f2d4cfc6c3f8102
LUCAS SASSO MACHADO <lucas_sasso-machado@estudante.sc.senai.br>
2022-10-28 21:22:44 -0300

    Classe de utilidades para data. Pelo momento ela serve só para delegar a função de checar por uma string vazia, checa por um valor nulo e por branco.

---
+++ site/src/main/java/br/com/projetoAplicadoIV/site/utils/DataUtils.java
@@ -0,0 +1,19 @@
+package br.com.projetoAplicadoIV.site.utils;
+
+public class DataUtils {
+    public static boolean isEmptyString(String string) {
+        //True is returned if it is null
+        if(isNullString(string) || isEmpty(string.trim())) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isNullString(Object string) {
+        return string == null;
+    }
+
+    public static boolean isEmpty(String string) {
+        return string.isEmpty();
+    }
+}
