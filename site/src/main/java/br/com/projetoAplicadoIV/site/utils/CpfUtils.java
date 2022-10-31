3cffd91607809ba3e55414f9e15ecb548e6928c0
LucasSM3006 <lucas.sasso@valtech.com>
2022-10-31 18:15:52 -0300

    CpfUtils é a classe de validações, é aonde a lógica da verificação do CPF fica, ou seja, é a classe responsável pela checagem DO CPF. Seria uma boa prática usar ela mais afundo caso tivessemos uma API, que daí tem o encapsulamento da API, mas isso não importa.

--- site/src/main/java/br/com/projetoAplicadoIV/site/utils/CpfUtils.java
+++ site/src/main/java/br/com/projetoAplicadoIV/site/utils/CpfUtils.java
@@ -1,58 +1,48 @@
 package br.com.projetoAplicadoIV.site.utils;
 
+import java.math.BigInteger;
+import java.util.Objects;
+
 public class CpfUtils {
-    private String message = "";
+
     public boolean verifyCpfLength(String cpf) {
-        if(cpf.length() != 11) return true;
+        return cpf.length() == 11;
+    }
+
+    public boolean validateCpf(String cpf) {
+        if(Objects.equals(cpf, "11111111111") || Objects.equals(cpf, "22222222222") ||
+                Objects.equals(cpf, "33333333333") || Objects.equals(cpf, "44444444444") ||
+                Objects.equals(cpf, "55555555555") || Objects.equals(cpf, "66666666666") ||
+                Objects.equals(cpf, "77777777777") || Objects.equals(cpf, "88888888888") ||
+                Objects.equals(cpf, "99999999999")) return false;
+
+        if(validateFirstDigit(cpf)) {
+            return validateSecondDigit(cpf);
+        }
         return false;
     }
 
-    public boolean verifyCpf(String cpf) {
+    private boolean validateFirstDigit(String cpf) {
+        int FIRST_VERIF_VALUE = 9; // Used to verify the digits and as the string indexes.
+        return validateDigit(cpf, FIRST_VERIF_VALUE);
+    }
+
+    private boolean validateSecondDigit(String cpf) {
+        int SECOND_VERIF_VALUE = 10; // Used to verify the digits and as the string indexes.
+        return validateDigit(cpf, SECOND_VERIF_VALUE);
+    }
+
+    private boolean validateDigit(String cpf, int counter) {
         int soma = 0;
-        double uh = 0.0;
-        double resto = 0.0;
-        double restoInt = 0;
+        double resto;
 
-        for(int i = 0; i < 9; i++) {
-            soma += Character.getNumericValue(cpf.charAt(i)) * (10 - i);
+        for(int i = 0; i < counter; i++) {
+            soma += Character.getNumericValue(cpf.charAt(i)) * ((counter + 1) - i);
         }
 
-        uh = soma * 10 / 11;
         resto = soma * 10 % 11;
-        if(resto >= 10) {
-            resto = 0;
-            restoInt = 0;
-        }
+        resto = resto >= 10 ? 0 : resto;
 
-        restoInt = Math.round(resto);
-
-        System.out.println(uh);
-        System.out.println(resto);
-
-
-
-        return false;
-    }
-
-    public boolean validateFirstDigit() {
-        return true;
-    }
-
-    public boolean validateSecondDigit() {
-        return true;
-    }
-
-    public void addToMessage(String message) {
-        this.message += message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        String msg = message;
-        setMessage("");
-        return msg;
+        return Character.getNumericValue(cpf.charAt(counter)) == resto;
     }
 }
