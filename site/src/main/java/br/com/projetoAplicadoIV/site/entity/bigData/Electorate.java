805c9dedd5a4e58427b83cb595fdc4b75a7d954a
Pedro Meerholz <developer.pedromeerholz@gmail.com>
2022-12-03 02:52:15 -0300

    Criação da entity de eleitorado (Electorate)

---
+++ site/src/main/java/br/com/projetoAplicadoIV/site/entity/bigData/Electorate.java
@@ -0,0 +1,61 @@
+package br.com.projetoAplicadoIV.site.entity.bigData;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="eleitorado")
+public class Electorate {
+    @Id
+    private int id;
+    @Column(name="genero")
+    private String gender;
+    @Column(name="estado_civil")
+    private String maritalStatus;
+    @Column(name="faixa_etaria")
+    private String ageGroup;
+    @Column(name="scholarity")
+    private String scholarity;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getMaritalStatus() {
+        return maritalStatus;
+    }
+
+    public void setMaritalStatus(String maritalStatus) {
+        this.maritalStatus = maritalStatus;
+    }
+
+    public String getAgeGroup() {
+        return ageGroup;
+    }
+
+    public void setAgeGroup(String ageGroup) {
+        this.ageGroup = ageGroup;
+    }
+
+    public String getScholarity() {
+        return scholarity;
+    }
+
+    public void setScholarity(String scholarity) {
+        this.scholarity = scholarity;
+    }
+}
